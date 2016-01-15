4513c3b6dfbbc39600259c7db6db3c7ef7a2388f
paulb <paulb@ashridgetech.com>
2016-01-15 14:57:31 +0000

    Deglitcher: working for simple diamond pattern

--- src/main/deglitcher/Deglitcher.js
+++ src/main/deglitcher/Deglitcher.js
@@ -4,23 +4,16 @@ const START = '__deglitcherStart__';
 const END = '__deglitcherEnd__';
 
 class RootDeglitcherObserver extends Rx.internals.AbstractObserver {
-    constructor(o, source) {
+    constructor(o) {
         super();
         this._o = o;
-        this.source = source;
-        this.targetIsDeglitcher = o.observer && o.observer instanceof DeglitcherSender
+        this.targetIsDeglitcher = o.observer && o.observer instanceof DeglitcherSender;
     }
 
     next(x) {
-
-        if (this.targetIsDeglitcher) {
-            this._o.onNext(START);
-        }
-
-        this._o.onNext(x);
-
-        if (this.targetIsDeglitcher) {
-            this._o.onNext(END);
+        let isControlEvent = x == START || x == END;
+        if (!isControlEvent || this.targetIsDeglitcher) {
+            this._o.onNext(x);
         }
     };
 
@@ -32,14 +25,14 @@ export class RootDeglitcherObservable extends Rx.ObservableBase {
     constructor(source) {
         super();
         this.source = source;
+        this.sharedSourceWithControlEvents = source.flatMap((x, i) => Rx.Observable.from([START, x, END])).share();
         // wrap all functions that return observables
         this.map = this._wrap(this.map);
     }
 
     subscribeCore (o) {
-        return this.source.subscribe(new RootDeglitcherObserver(o, this));
+        return this.sharedSourceWithControlEvents.subscribe(new RootDeglitcherObserver(o));
     };
-
 
     _wrap(originalFn) {
         let self = this;
@@ -58,6 +51,7 @@ export class DeglitcherObservable extends Rx.ObservableBase {
         let targets = sources.map((s) => new DeglitcherTarget(this, s));
         this.originalObservable = originalObservableCreator(targets);
         this.map = this._wrap(this.map);
+        this.combineLatest = this._wrapCombineLatest(this.combineLatest);
     }
 
     subscribeCore (o) {
@@ -75,6 +69,15 @@ export class DeglitcherObservable extends Rx.ObservableBase {
         return function(fn, thisArg) {
             let wrappedObservableCreator = function(targets) { return originalFn.call(targets[0], fn, thisArg) };
             let result = new DeglitcherObservable(wrappedObservableCreator, self);
+            return result;
+        }
+    }
+
+    _wrapCombineLatest(originalFn) {
+        let self = this;
+        return function(other, combineFn) {
+            let wrappedObservableCreator = function(targets) { return originalFn.call(targets[0], targets[1], combineFn) };
+            let result = new DeglitcherObservable(wrappedObservableCreator, self, other);
             return result;
         }
     }
@@ -103,7 +106,6 @@ class DeglitcherSender extends Rx.internals.AbstractObserver {
     }
 
     next(x) {
-        console.log('DeglitcherSender next', x);
         if (x == START || x == END) {
             this.receiver.onNext(x)
         } else {
@@ -127,7 +129,6 @@ class DeglitcherReceiver extends Rx.internals.AbstractObserver {
     }
 
     next(x) {
-        console.log('DeglitcherReceiver next', x);
         if (x == START) {
             this.expected++;
             this.targetIsDeglitcher && this._o.onNext(x);
--- src/test/deglitcher/DeglitcherSpec.js
+++ src/test/deglitcher/DeglitcherSpec.js
@@ -24,26 +24,49 @@ describe('Deglitcher', function () {
     });
 
     it('passes through value with wrapped map', () => {
-        let source = Rx.Observable.range(1, 3);
+        let source = new Rx.Subject();
         let result = [];
 
         let deg = new RootDeglitcherObservable(source);
         let degMapped = deg.map( double );
         degMapped.subscribe(x => {result.push(x)});
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
         result.should.eql([2, 4, 6]);
     });
 
     it('passes through value with two wrapped maps', () => {
-        let source = Rx.Observable.range(1, 3);
+        let source = new Rx.Subject();
         let result = [];
 
         let deg = new RootDeglitcherObservable(source);
         let degMapped1 = deg.map( double );
         let degMapped2 = degMapped1.map( plus10 );
         degMapped2.subscribe(x => {result.push(x)});
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
         result.should.eql([12, 14, 16]);
     });
 
+    it('emits one event for diamond pattern', () => {
+        let source = new Rx.Subject();
+        let result = [];
+
+        let deg = new RootDeglitcherObservable(source);
+        let degMappedDouble = deg.map( double );
+        let degMappedPlus10 = deg.map( plus10 );
+        let degCombined = degMappedDouble.combineLatest(degMappedPlus10, (a, b) => a + b);
+        degCombined.subscribe(x => {result.push(x)});
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
+        result.should.eql([13, 16, 19]);
+    });
 
 
 });
