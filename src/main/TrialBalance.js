a685f76ab773df78fc89239597c14442b95d90d1
paulb <paulb@ashridgetech.com>
2016-01-23 10:36:34 +0000

    Slightly refined trial balance

--- src/main/TrialBalance.js
+++ src/main/TrialBalance.js
@@ -1,4 +1,5 @@
 import Rx from 'rx';
+import {DeglitcherObservable, RootDeglitcherObservable} from 'deglitcher';
 const combineLatest = Rx.Observable.combineLatest;
 
 
@@ -11,21 +12,22 @@ export default class TrialBalance {
 
     // stream of set of account summary with name, balance
     get accountBalances() {
-        let summaryList = this.accountList.map(accts =>
-            accts.map( a => a.summary )
-        );
+        let summaryList = this.accountList.map(function (accts) {
+            return combineLatest(accts.map(a => a.summary));
+        });
 
-        let summaryLatest = summaryList.map( (list) => combineLatest(list));
-        let result = summaryLatest.switch();
+        let result = summaryList.switch();
         return result;
     }
 
     get balanceTotals() {
-        return accountBalances().map( (summaryList) => {
+        return accountBalances().map((summaryList) => {
             let slStream = Rx.fromArray(summaryList);
             let debit = slStream.pluck('debitBalance').sum();
             let credit = slStream.pluck('creditBalance').sum();
-            {debit, credit}
+            {
+                debit, credit
+            }
         });
 
 
